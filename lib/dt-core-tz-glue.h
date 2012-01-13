b25e8a669fdee61944b74c2b06af7811dd522347
Sebastian Freundt <freundt@ga-group.nl>
2012-01-13 20:17:12 +0000

    minor, add dt-core-tz-glue.[ch] with an example implementation ... for dtz_forgetz()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ lib/dt-core-tz-glue.h
@@ -0,0 +1,76 @@
+/*** dt-core-tz-glue.h -- glue between tzraw and dt-core
+ *
+ * Copyright (C) 2012 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of dateutils.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **/
+#if !defined INCLUDED_dt_core_tz_glue_h_
+#define INCLUDED_dt_core_tz_glue_h_
+
+/* include the guys we need gluing, innit */
+#include "dt-core.h"
+#include "tzraw.h"
+
+#if defined __cplusplus
+extern "C" {
+#endif	/* __cplusplus */
+
+#if defined INCLUDE_DATETIME_CORE_IMPL && defined INCLUDE_TZRAW_IMPL
+# define INCLUDE_DT_CORE_TZ_GLUE_IMPL
+#endif	/* INCLUDE_DATETIME_CORE_IMPL && INCLUDE_TZRAW_IMPL */
+#if !defined DECLF
+# define DECLF	static __attribute__((unused))
+# define DEFUN	static
+#eldif !defined DEFUN
+# define DEFUN
+#endif	/* !DECLF */
+#if !defined restrict
+# define restrict	__restrict
+#endif	/* !restrict */
+
+
+#if defined INCLUDE_DT_CORE_TZ_GLUE_IMPL
+# include "dt-core-tz-glue.c"
+#endif	/* INCLUDE_DT_CORE_TZ_GLUE_IMPL */
+
+
+/* decls */
+/**
+ * Return a dt object that forgot about DT's zone and uses ZONE instead. */
+DECLF struct dt_dt_s dtz_forgetz(struct dt_dt_s dt, zif_t zone);
+
+#if defined __cplusplus
+}
+#endif	/* __cplusplus */
+
+#endif	/* INCLUDED_dt_core_tz_glue_h_ */
