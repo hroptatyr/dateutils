e9b87b142625340405fbbbcb4219adfba0e89678
Sebastian Freundt <freundt@ga-group.nl>
2012-03-23 17:18:27 +0000

    minor, if byteswap.h is there, swap the bytes ourselves
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/boobs.h
+++ lib/boobs.h
@@ -48,46 +48,49 @@
 # include <sys/endian.h>
 #elif defined HAVE_BYTEORDER_H
 # include <byteorder.h>
-#elif defined HAVE_ARPA_INET_H
-# include <arpa/inet.h>
-#elif defined NETINET_IN_H
-# include <netinet/in.h>
-#else
-/* FUCK!!! */
-#endif	/* HAVE_ENDIAN_H */
+#endif	/* ENDIAN_H || SYS/ENDIAN_H || BYTEORDER_H */
+
+/* check for byteswap to do the swapping ourselves if need be */
+#if defined HAVE_BYTESWAP_H
+# include <byteswap.h>
+#endif	/* BYTESWAP_H */
 
 /* and even now we may be out of luck */
 #if !defined be32toh
 # if defined betoh32
 #  define be32toh	betoh32
-# elif defined ntohl
-#  define be32toh	ntohl
 # elif defined WORDS_BIGENDIAN || __BYTE_ORDER == __BIG_ENDIAN
 #  define be32toh(x)	(x)
+# elif __BYTE_ORDER == __LITTLE_ENDIAN
+#  define be32toh(x)	__bswap_32(x)
 # endif
 #endif	/* !be32toh */
 
 #if !defined le32toh
 # if defined letoh32
 #  define le32toh	letoh32
-# elif !defined WORDS_BIGENDIAN || __BYTE_ORDER == __LITTLE_ENDIAN
+# elif defined WORDS_BIGENDIAN || __BYTE_ORDER == __BIG_ENDIAN
+#  define le32toh(x)	__bswap_32(x)
+# elif __BYTE_ORDER == __LITTLE_ENDIAN
 #  define le32toh(x)	(x)
 # endif	 /* letoh32 */
 #endif	/* !le32toh */
 
 #if !defined htobe32
-# if defined htonl
-#  define htobe32	htonl
-# elif defined WORDS_BIGENDIAN || __BYTE_ORDER == __BIG_ENDIAN
+# if defined WORDS_BIGENDIAN || __BYTE_ORDER == __BIG_ENDIAN
 #  define htobe32(x)	(x)
+# elif __BYTE_ORDER == __LITTLE_ENDIAN
+#  define htobe32(x)	__bswap_32(x)
 # endif
 #endif	/* !be32toh */
 
 #if !defined htole32
-# if !defined WORDS_BIGENDIAN || __BYTE_ORDER == __LITTLE_ENDIAN
+# if defined WORDS_BIGENDIAN || __BYTE_ORDER == __BIG_ENDIAN
+#  define htole32(x)	__bswap_32(x)
+# if __BYTE_ORDER == __LITTLE_ENDIAN
 #  define htole32(x)	(x)
 # endif
-#endif	/* !le32toh */
+#endif	/* !htole32 */
 
 /* we could technically include byteswap.h and to the swap ourselves
  * in the missing cases.  Instead we'll just leave it as is and wait
