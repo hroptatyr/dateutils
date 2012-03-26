f842f3f40c42fdeffb014469423982f54fa8dfa2
Sebastian Freundt <freundt@ga-group.nl>
2012-03-26 10:40:49 +0000

    test, provide tests to check for the date/time type enumerations
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ test/enum-1.c
@@ -0,0 +1,36 @@
+#include "dt-core.h"
+
+int
+main(void)
+{
+	int res = 0;
+
+	if (DT_SANDWICH != DT_NTYP) {
+		fprintf(stderr, "DT_SANDWICH %hu != DT_NTYP %hu\n",
+			DT_SANDWICH, DT_NTYP);
+		res = 1;
+	}
+	if (DT_SANDWICH_D != DT_SANDWICH + 1) {
+		fprintf(stderr, "DT_SANDWICH_D %hu != DT_SANDWICH + 1 %hu\n",
+			DT_SANDWICH_D, DT_SANDWICH + 1);
+		res = 1;
+	}
+	if (DT_SANDWICH_T != DT_SANDWICH + 2) {
+		fprintf(stderr, "DT_SANDWICH_T %hu != DT_SANDWICH + 2 %hu\n",
+			DT_SANDWICH_T, DT_SANDWICH + 2);
+		res = 1;
+	}
+	if (DT_PACK <= DT_NTYP) {
+		fprintf(stderr, "DT_PACK %hu <= DT_NTYP %hu\n",
+			DT_PACK, DT_NTYP);
+		res = 1;
+	}
+	if (DT_SEXY <= DT_NTYP) {
+		fprintf(stderr, "DT_SEXY %hu <= DT_NTYP %hu\n",
+			DT_SEXY, DT_NTYP);
+		res = 1;
+	}
+	return res;
+}
+
+/* enum-1.c ends here */
