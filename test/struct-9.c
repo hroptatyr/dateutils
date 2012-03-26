137b6b59f06148b5b090a039bf00d72741a8890c
Sebastian Freundt <freundt@ga-group.nl>
2012-03-26 07:59:29 +0000

    test, provide basic structure tests
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ test/struct-9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "time-core.h"
+
+int
+main(void)
+{
+	int res = 0;
+
+#define CHECK_SIZE(x, y)						\
+	if (sizeof(x) != sizeof(y)) {					\
+		fprintf(stderr, "sizeof(" #x ") -> %zu\n", sizeof(x));	\
+		res = 1;						\
+	}
+
+	CHECK_SIZE(struct dt_t_s, uint64_t);
+	CHECK_SIZE(dt_hms_t, uint64_t);
+	return res;
+}
+
+/* struct-9.c ends here */
