8357b691124f42821956b24c35333327fcee60da
Sebastian Freundt <freundt@ga-group.nl>
2012-04-02 10:23:38 +0000

    fix, hack, honour the ambiguous `m' duration (interpret as `mo') in dt_dtadd()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/dt-core.c
+++ lib/dt-core.c
@@ -686,9 +686,9 @@ dt_strpdtdur(const char *str, char **ep)
 		res.t.sdur = d.st.h * SECS_PER_HOUR +
 			d.st.m * SECS_PER_MIN +
 			d.st.s;
-		/* but also put the a note in the ymd slot */
-		if (d.sd.m) {
-			res.d.ymd.m = d.sd.m;
+		/* but also put the a note in the md slot */
+		if (!d.st.h && !d.st.s && d.sd.m) {
+			res.d.md.m = d.sd.m;
 		}
 
 	} else {
@@ -952,9 +952,13 @@ DEFUN struct dt_dt_s
 dt_dtadd(struct dt_dt_s d, struct dt_dt_s dur)
 {
 	signed int carry = 0;
-	dt_dttyp_t typ;
+	dt_dttyp_t typ = d.typ;
 
-	if (dur.t.dur) {
+	if (UNLIKELY(dur.t.dur && dt_sandwich_only_d_p(d))) {
+		/* probably +/-[n]m where `m' was meant to be `mo' */
+		dur.d.typ = DT_MD;
+		goto dadd;
+	} else if (dur.t.dur) {
 		d.t = __tadd(d.t, dur.t, &carry);
 	}
 
@@ -973,7 +977,8 @@ dt_dtadd(struct dt_dt_s d, struct dt_dt_s dur)
 	}
 
 	/* demote D's and DUR's type temporarily */
-	if ((typ = d.typ) != DT_SANDWICH_UNK && dur.d.typ != DT_DUNK) {
+	if (d.typ != DT_SANDWICH_UNK && dur.d.typ != DT_DUNK) {
+	dadd:
 		/* let date-core do the addition */
 		d.d = dt_dadd(d.d, dur.d);
 	} else if (dur.d.typ != DT_DUNK) {
