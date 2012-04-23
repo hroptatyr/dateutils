be8c3f15f98d2c2e3808f502b48830e4a3a85ed2
Sebastian Freundt <freundt@ga-group.nl>
2012-04-23 12:33:15 +0000

    cosmetics, use nested initialiser for t-slot in dt_dt_initialiser()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/dt-core.h
+++ lib/dt-core.h
@@ -259,10 +259,12 @@ dt_dt_initialiser(void)
 		.dur = 0U,
 		.neg = 0U,
 		.u = 0U,
-		.t.typ = DT_TUNK,
-		.t.dur = 0U,
-		.t.neg = 0U,
-		.t.u = 0U
+		.t = {
+			.typ = DT_TUNK,
+			.dur = 0U,
+			.neg = 0U,
+			.u = 0U,
+		},
 	};
 #else  /* !__C1X */
 	struct dt_dt_s res;
