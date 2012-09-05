3361d15e93946e98bc860afb00a2454e2205d76f
Sebastian Freundt <freundt@ga-group.nl>
2012-09-05 11:01:30 +0000

    test, bring test for __ymd_get_wday() that could be used as performance test
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ test/basic_ymd_get_wday.c
@@ -0,0 +1,38 @@
+#include "dt-core.h"
+
+static unsigned int
+super(void)
+{
+	dt_ymd_t x = {0};
+	unsigned int super = 0;
+
+	for (int y = 1917; y < 4096; y++) {
+		for (int m = 1; m <= 12; m ++) {
+			for (int d = 1; d <= 28; d++) {
+				dt_dow_t w;
+				x.y = y;
+				x.m = m;
+				x.d = d;
+				w = __ymd_get_wday(x);
+				super += w;
+			}
+		}
+	}
+	return super;
+}
+
+int
+main(void)
+{
+	unsigned int supersum = 0;
+
+	for (size_t i = 0; i < 1024; i++) {
+		supersum += super();
+	}
+	if (supersum != 2249146368U) {
+		return 1;
+	}
+	return 0;
+}
+
+/* basic_ymd_get_wday.c ends here */
