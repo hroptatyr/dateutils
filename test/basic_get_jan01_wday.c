9211d2a66e1d84f5376d6da8dcae958730533127
Sebastian Freundt <freundt@ga-group.nl>
2012-09-05 12:37:21 +0000

    test, bring test for __get_jan01_wday() that could be used as performance test
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ test/basic_get_jan01_wday.c
@@ -0,0 +1,30 @@
+#include "dt-core.h"
+
+static unsigned int
+super(unsigned int super)
+{
+	for (int y = 1917; y < 4096; y++) {
+		dt_dow_t w;
+		w = __get_jan01_wday(y);
+		super += w;
+	}
+	return super;
+}
+
+int
+main(void)
+{
+	unsigned int supersum = 0;
+
+	for (size_t i = 0; i < 1024 * 1024; i++) {
+		supersum += super(supersum);
+	}
+	printf("super %u\n", supersum);
+	if (supersum != 4294960763U/*28y cycle*/ &&
+	    supersum != 4294960773U/*400y cycle*/) {
+		return 1;
+	}
+	return 0;
+}
+
+/* basic_get_jan01_wday.c ends here */
