355c081c5dd34bb83a13fefb4afdfc685f505449
Sebastian Freundt <freundt@ga-group.nl>
2012-09-10 07:37:53 +0000

    test, check built dseq and ddiff in dseq-cnt.1 test
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/dseq-cnt.1.sh
+++ test/dseq-cnt.1.sh
@@ -3,11 +3,16 @@
 BEG="1917-01-01"
 END="4095-12-31"
 
+TOOLDIR="$(pwd)/../src"
+
+DSEQ="${TOOLDIR}/dseq"
+DDIFF="${TOOLDIR}/ddiff"
+
 foo=`mktemp "/tmp/tmp.XXXXXXXXXX"`
 bar=`mktemp "/tmp/tmp.XXXXXXXXXX"`
 
-dseq "${BEG}" "${END}" | head -n-1 | wc -l > "${foo}"
-ddiff "${BEG}" "${END}" > "${bar}"
+"${DSEQ}" "${BEG}" "${END}" | head -n-1 | wc -l > "${foo}"
+"${DDIFF}" "${BEG}" "${END}" > "${bar}"
 
 diff "${foo}" "${bar}"
 rc=${?}
