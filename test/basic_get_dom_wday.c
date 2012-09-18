3a5685a9bfe986514345694dcabf8c2467459519
Sebastian Freundt <freundt@ga-group.nl>
2012-09-18 08:50:35 +0000

    test, provide (performance) test for __get_dom_wday()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ test/basic_get_dom_wday.c
@@ -0,0 +1,34 @@
+#include "dt-core.h"
+
+static unsigned int
+super(void)
+{
+	unsigned int super = 0;
+
+	for (int y = 1917; y < 2199; y++) {
+		for (int m = 1; m <= 12; m ++) {
+			for (int d = 1; d <= 28; d++) {
+				dt_dow_t w = __get_dom_wday(y, m, d);
+				super += y * m * w + d;
+			}
+		}
+	}
+	return super;
+}
+
+int
+main(void)
+{
+	unsigned int supersum = 0;
+
+	for (size_t i = 0; i < 512; i++) {
+		supersum += super();
+	}
+	printf("super %u\n", supersum);
+	if (supersum != 1486417920U) {
+		return 1;
+	}
+	return 0;
+}
+
+/* basic_get_wday.c ends here */
