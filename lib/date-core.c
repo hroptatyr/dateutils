63f10551eb1a208df11ea786255bb42ac4be8c46
Sebastian Freundt <freundt@ga-group.nl>
2012-11-02 11:20:18 +0000

    minor, hygiene, convert struct dt_d_s initialisation to initialiser in dt_strpddur()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/date-core.c
+++ lib/date-core.c
@@ -973,10 +973,10 @@ DEFUN struct dt_d_s
 dt_strpddur(const char *str, char **ep)
 {
 /* at the moment we allow only one format */
-	struct dt_d_s res = {DT_DUNK};
+	struct dt_d_s res = dt_d_initialiser();
+	struct strpd_s d = strpd_initialiser();
 	const char *sp = str;
 	int tmp;
-	struct strpd_s d = strpd_initialiser();
 
 	if (str == NULL) {
 		goto out;
