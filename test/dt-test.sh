a963f071e3e40e5e87af724021f7488f4b59a23d
Sebastian Freundt <freundt@ga-group.nl>
2012-12-21 09:16:33 +0000

    minor, migrate to C-based (getopt-long based) test compiler, dt-test
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/dt-test.sh
+++ test/dt-test.sh
@@ -1,55 +1,27 @@
 #!/bin/bash
 
-usage()
+## should be called by ut-test
+if test -z "${testfile}"; then
+	exit 1
+fi
+
+## helper funs that might be of use in the test files
+xrealpath()
 {
-	cat <<EOF 
-$(basename ${0}) [OPTION] TEST_FILE
-
---builddir DIR  specify where tools can be found
---srcdir DIR    specify where the source tree resides
---hash PROG     use hasher PROG instead of md5sum
---husk PROG     use husk around tool, e.g. 'valgrind -v'
-
--h, --help      print a short help screen
-EOF
+	readlink -f "${1}" 2>/dev/null || \
+	realpath "${1}" 2>/dev/null || \
+	(
+		cd $(dirname "${1}") || exit 1
+		tmp_target=$(basename "${1}")
+		# Iterate down a (possible) chain of symlinks
+		while test -L "${tmp_target}"; do
+			tmp_target=$(readlink "${tmp_target}")
+			cd $(dirname "${tmp_target}") || exit 1
+			tmp_target=$(basename "${tmp_target}")
+		done
+		echo "$(pwd -P || pwd)/${tmp_target}"
+	) 2>/dev/null
 }
-
-for arg; do
-	case "${arg}" in
-	"-h"|"--help")
-		usage
-		exit 0
-		;;
-	"--builddir")
-		builddir="${2}"
-		shift 2
-		;;
-	"--srcdir")
-		srcdir="${2}"
-		shift 2
-		;;
-	"--hash")
-		hash="${2}"
-		shift 2
-		;;
-	"--husk")
-		HUSK="${2}"
-		shift 2
-		;;
-	--)
-		shift
-		testfile="${1}"
-		break
-		;;
-	"-"*)
-		echo "unknown option ${arg}" >&2
-		shift
-		;;
-	*)
-		testfile="${1}"
-		;;
-	esac
-done
 
 ## setup
 fail=0
@@ -88,23 +60,6 @@ myexit()
 		mv "gmon.out" "${runnm}"
 	fi
 	exit ${1:-1}
-}
-
-xrealpath()
-{
-	readlink -f "${1}" 2>/dev/null || \
-	realpath "${1}" 2>/dev/null || \
-	(
-		cd $(dirname "${1}") || exit 1
-		tmp_target=$(basename "${1}")
-		# Iterate down a (possible) chain of symlinks
-		while test -L "${tmp_target}"; do
-			tmp_target=$(readlink "${tmp_target}")
-			cd $(dirname "${tmp_target}") || exit 1
-			tmp_target=$(basename "${tmp_target}")
-		done
-		echo "$(pwd -P || pwd)/${tmp_target}"
-	) 2>/dev/null
 }
 
 find_file()
@@ -163,7 +118,7 @@ stdin=$(find_file "${stdin}")
 stdout=$(find_file "${stdout}")
 stderr=$(find_file "${stderr}")
 
-eval_echo "${HUSK}" "${TOOL}" "${CMDLINE}" \
+eval_echo "${husk}" "${TOOL}" "${CMDLINE}" \
 	< "${stdin:-/dev/null}" \
 	3>&2 \
 	> "${tool_stdout}" 2> "${tool_stderr}" || fail=${?}
