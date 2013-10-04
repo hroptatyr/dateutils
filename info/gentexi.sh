03dbc8cade9a246e5cda6b380108e4be0ac6acae
Sebastian Freundt <freundt@ga-group.nl>
2013-10-04 10:46:35 +0000

    chore, adapt gentexi.sh script to clit files
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- info/gentexi.sh
+++ info/gentexi.sh
@@ -34,71 +34,13 @@ if test "${#}" -eq 0; then
 	exit 0
 fi
 
-## backport from new test suite
-tsp_create_env()
-{
-	TS_TMPDIR="`basename "${BINARY}"`.tmpd"
-	rm -rf "${TS_TMPDIR}" || return 1
-	mkdir "${TS_TMPDIR}" || return 1
-
-	TS_STDIN="${TS_TMPDIR}/stdin"
-	TS_EXP_STDOUT="${TS_TMPDIR}/exp_stdout"
-	TS_EXP_STDERR="${TS_TMPDIR}/exp_stderr"
-	TS_OUTFILE="${TS_TMPDIR}/tool_outfile"
-	TS_EXP_EXIT_CODE="0"
-	TS_DIFF_OPTS=""
-
-	tool_stdout="${TS_TMPDIR}/tool_stdout"
-	tool_stderr="${TS_TMPDIR}/tool_sterr"
-}
-
-tsp_reset_env()
-{
-	zero()
-	{
-		dd if=/dev/zero of="${1}" count=0 status=noxfer 2>/dev/null
-	}
-
-	zero "${TS_STDIN}"
-	zero "${TS_EXP_STDOUT}"
-	zero "${TS_EXP_STDERR}"
-	zero "${TS_OUTFILE}"
-
-	zero "${tool_stdout}"
-	zero "${tool_stderr}"
-}
-
-## now go for examples
-myexit()
-{
-	if test "${1}" = "0"; then
-		rm -rf "${TS_TMPDIR}"
-	fi
-	exit ${1:-1}
-}
-
-## setup
-fail=0
-tsp_create_env || myexit 1
-
-. $(dirname "${0}")"/genex"
-
 echo "@section Examples"
 for i; do
-	tsp_reset_env
-
-	. "${i}"
-
-	## double check we're in the right tool
-	if test "${TOOL}" != "${BINNAME}"; then
-		continue
-	fi
-
 	echo
 	echo "@example"
-	genex "${BINARY}" "${BINNAME}" "${CMDLINE}" "${TS_STDIN}" | \
-		sed 's/@/@@/g; s/{/@{/g; s/}/@}/g'
+	sed '/^#!/d; /ends here$/d; /^[ \t]*$/d;
+s/@/@@/g; s/{/@{/g; s/}/@}/g' "${i}"
 	echo "@end example"
 done
 
-myexit 0
+exit 0
