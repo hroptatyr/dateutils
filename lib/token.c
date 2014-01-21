2bd5696f3135bdee2a04137c57fec1c57a238eb8
Sebastian Freundt <freundt@ga-group.nl>
2014-01-21 11:53:30 +0000

    minor, actually parse the 0 and the SPC modifier in __tok_spec()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/token.c
+++ lib/token.c
@@ -174,6 +174,14 @@ next:
 		/* roman numerals modifier */
 		res.rom = 1;
 		goto next;
+	case '0':
+		/* 0 padding modifier */
+		res.pad0 = 1U;
+		goto next;
+	case ' ':
+		/* SPC padding modifier */
+		res.padspc = 1U;
+		goto next;
 	case 'r':
 		/* real modifier */
 		res.tai = 1;
