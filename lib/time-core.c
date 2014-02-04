3dbb06ed36084ee1e52271f106848faed6af691c
Sebastian Freundt <freundt@ga-group.nl>
2014-02-04 08:59:01 +0000

    chore, include config.h in time-core.c
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/time-core.c
+++ lib/time-core.c
@@ -38,6 +38,9 @@
 #if !defined INCLUDED_time_core_c_
 #define INCLUDED_time_core_c_
 
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
