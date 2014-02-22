0983790b09fe669c337370857f6606a64d252ce7
Sebastian Freundt <freundt@ga-group.nl>
2014-02-22 06:54:37 +0000

    fix, PEBKAC, always take over zdiff info from any dt_dt_s upon dt_strfdt()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- lib/dt-core.c
+++ lib/dt-core.c
@@ -789,8 +789,9 @@ dt_strfdt(char *restrict buf, size_t bsz, const char *fmt, struct dt_dt_s that)
 		d.st.m = that.t.hms.m;
 		d.st.s = that.t.hms.s;
 		d.st.ns = that.t.hms.ns;
-		d.zdiff = zdiff_sec(that);
 	}
+	/* make sure we always snarf the zdiff info */
+	d.zdiff = zdiff_sec(that);
 
 	/* assign and go */
 	bp = buf;
