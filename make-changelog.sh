c93ca48c1449f6a6bb58fe2bbfa51e3c90e4f9b4
Sebastian Freundt <freundt@ga-group.nl>
2014-02-24 09:52:06 +0000

    also leave beta versions out
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- make-changelog.sh
+++ make-changelog.sh
@@ -10,5 +10,6 @@ logo: dateutils_logo_120.png
 
 EOF
 
-for i in `git tag | grep -vF 'rc' | tac`; \
+for i in `git tag | grep -vF 'rc
+beta' | tac`; \
     git tag -l -n1000 ${i} | "$(dirname "${0}")/make-changelog.sed"
