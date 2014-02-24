59787e5ea2bfddb497f02f5cf72527ae5653127d
Sebastian Freundt <hroptatyr@fresse.org>
2014-02-24 06:45:56 +0000

    polish changelog maker a little
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

--- make-changelog.sh
+++ make-changelog.sh
@@ -10,4 +10,4 @@ logo: dateutils_logo_120.png
 
 EOF
 
-for i in `git tag | grep -vF 'rc' | tac`; git tag -l -n1000 ${i} | sed 's/v\([0-9]\.[.0-9]*\)  */\nv\1\n=======\n    /'
+for i in `git tag | grep -vF 'rc' | tac`; git tag -l -n1000 ${i} | sed 's/v\([0-9]\.[.0-9]*\)  */\nv\1\n=======\n/; /Keywords:/d; s/^    //; s/^  /    /'
