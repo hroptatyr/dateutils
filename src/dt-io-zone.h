0c4db6008d5281c4b2abd4776727285962b288be
Sebastian Freundt <freundt@ga-group.nl>
2014-08-14 10:49:47 +0000

    hygiene, update copyright years

--- src/dt-io-zone.h
+++ src/dt-io-zone.h
@@ -1,6 +1,6 @@
 /*** dt-io-zone.h -- abstract from raw zone interface
  *
- * Copyright (C) 2010-2014 Sebastian Freundt
+ * Copyright (C) 2009-2014 Sebastian Freundt
  *
  * Author:  Sebastian Freundt <freundt@ga-group.nl>
  *
