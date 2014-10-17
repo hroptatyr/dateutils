3cf6e4d0f69482cc36dc926886879ba2f0c98f07
Sebastian Freundt <freundt@ga-group.nl>
2014-10-17 12:32:49 +0000

    hack, use ^ '0' to convert to and from ascii numbers

--- lib/strops.c
+++ lib/strops.c
@@ -75,7 +75,7 @@ strtoi_lim(const char *str, const char **ep, int32_t llim, int32_t ulim)
 	for (sp = str, rulim = ulim > 10 ? ulim : 10;
 	     rulim && *sp >= '0' && *sp <= '9' && (res *= 10) <= ulim;
 	     sp++, rulim /= 10) {
-		res += *sp - '0';
+		res += *sp ^ '0';
 	}
 	if (UNLIKELY(sp == str)) {
 		res = -1;
@@ -109,7 +109,7 @@ DEFUN size_t
 ui32tostr(char *restrict buf, size_t bsz, uint32_t d, int width)
 {
 /* all strings should be little */
-#define C(x, d)	(char)((x) / (d) % 10 + '0')
+#define C(x, d)	(char)(((x) / (d) % 10) ^ '0')
 	size_t res;
 
 	if (UNLIKELY(d > 1000000000U)) {
@@ -150,7 +150,7 @@ DEFUN size_t
 ui32topstr(char *restrict b, size_t z, uint32_t d, int width, char pad)
 {
 /* all strings should be little */
-#define C(x)	(char)((x) + '0')
+#define C(x)	(char)((x) ^ '0')
 	char *restrict bp = b;
 	const char *const ep = b + z;
 
