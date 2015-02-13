cb83ba0aac88c5668f0c0f165670dba17f1c5e72
Sebastian Freundt <freundt@ga-group.nl>
2015-02-13 08:10:41 +0100

    minor, allow strptime(1) behaviour based on the locale through -l|--locale

--- src/strptime.c
+++ src/strptime.c
@@ -45,6 +45,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
+#include <locale.h>
 
 #include "dt-io.h"
 #include "prchunk.h"
@@ -169,6 +170,13 @@ main(int argc, char *argv[])
 	}
 	/* get quiet predicate */
 	quietp = argi->quiet_flag;
+
+	/* get locale flag */
+	if (!argi->locale_flag) {
+		setlocale(LC_TIME, "C");
+	} else {
+		setlocale(LC_TIME, "");
+	}
 
 	/* get lines one by one, apply format string and print date/time */
 	if (ninput == 0) {
