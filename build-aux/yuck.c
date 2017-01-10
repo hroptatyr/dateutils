c97bc02460420caae6865e15fd4cdb991627e1a6
Sebastian Freundt <freundt@ga-group.nl>
2017-01-10 04:19:52 +0000

    fix, fix build issue on FreeBSD 11 as proposed in bug #54

--- build-aux/yuck.c
+++ build-aux/yuck.c
@@ -61,6 +61,8 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #include <time.h>
 #if defined WITH_SCMVER
 # include <yuck-scmver.h>
